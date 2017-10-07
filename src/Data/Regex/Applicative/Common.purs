038227376cbd70922c4306b83b031865bdb4857f
Sam Thomson <sammthomson@gmail.com>
2017-10-06 21:16:46 -0700

    add back `digit`, `decimal`

--- src/Data/Regex/Applicative/Common.purs
+++ src/Data/Regex/Applicative/Common.purs
@@ -1,26 +1,27 @@
 -- |
 -- Collection of commonly used regular expressions.
 module Data.Regex.Applicative.Common (
-    -- * Digits
-    -- digit
-    hexDigit
-    -- * Numbers
-  , signed
-  -- , decimal
-  , hexadecimal
+  -- * Digits
+  digit,
+  hexDigit,
+  -- * Numbers
+  signed,
+  decimal,
+  hexadecimal
 ) where
 
 -- import Data.Char
 import Control.Alt ((<|>))
-import Data.Char.Unicode (digitToInt)
+import Data.Char.Unicode (digitToInt, isDigit)
 import Data.Foldable (foldl)
+import Data.Maybe (Maybe(..))
 import Data.Regex.Applicative (RE, msym, some, sym)
 import Prelude (class Ring, id, negate, pure, (*), (+), (<$), (<$>), (<*>))
 
 
--- -- | Decimal digit, i.e. @\'0\'@..@\'9\'@
--- digit :: RE Char Int
--- digit = digitToInt <$> psym isDigit
+-- | Decimal digit, i.e. @\'0\'@..@\'9\'@
+digit :: RE Char Int
+digit = msym (\c -> if isDigit c then digitToInt c else Nothing)
 
 -- | Hexadecimal digit
 -- i.e. @\'0\'@..@\'9\'@, @\'a\'@..@\'f\'@, @\'A\'@..@\'F\'@.
@@ -35,9 +36,9 @@ signed p = sign <*> p
         <|> negate <$ sym '-'
         <|> pure id
 
--- -- | Parse decimal number without sign.
--- decimal :: RE Char Int
--- decimal = foldl (\d i -> d * 10 + i) 0 <$> some digit
+-- | Parse decimal number without sign.
+decimal :: RE Char Int
+decimal = foldl (\d i -> d * 10 + i) 0 <$> some digit
 
 -- | Parse decimal number without sign.
 hexadecimal :: RE Char Int
